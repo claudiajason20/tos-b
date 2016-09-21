c028edb2ab7ec2d9c53dcffce01751eabd5c5ed7
Claudia <m26415134@joh.petra.ac.id>
2016-09-21 23:02:30 +0700

    ralat

--- lab03-26415134.sh
+++ lab03-26415134.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 beli=`curl -s http://www.bankmandiri.co.id/ | grep USD -A2 | cut -d ">" -f2 | cut "<" -f1 | xargs | cut -d " " -f2`
 jual=`curl -s http://www.bankmandiri.co.id/ | grep USD -A2 | cut -d ">" -f2 | cut "<" -f1 | xargs | cut -d " " -f2`
-echo "Harga Jual: $jual"
-echo "Harga beli: $beli"
+echo "`date` | $jual | $beli"
